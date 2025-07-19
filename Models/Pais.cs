97cee5ce013fbd66fed489e187c7a012c3025aeb
Omar Genao <omargenao78@gmail.com>
2025-07-19 13:14:21 -0400

    creacion de modelos

---
+++ Models/Pais.cs
@@ -0,0 +1,9 @@
+﻿namespace AgenciaDeToursFinal.Models
+{
+    public class Pais
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public ICollection<Destino> Destinos { get; set; }
+    }
+}
